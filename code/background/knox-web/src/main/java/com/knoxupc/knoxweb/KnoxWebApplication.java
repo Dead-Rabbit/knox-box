96086266627eab21276c8e206b5a4f11966d7362
= <joe@JOE00.local>
2018-07-19 15:53:50 +0800

    加入iview-admin

---
+++ code/background/knox-web/src/main/java/com/knoxupc/knoxweb/KnoxWebApplication.java
@@ -0,0 +1,13 @@
+
+package com.knoxupc.knoxweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoxWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnoxWebApplication.class, args);
+    }
+}
