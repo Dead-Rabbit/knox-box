196db328de820325c3a8ba94f91565a2542e2cd3
= <joe@JOE00.local>
2018-07-21 18:12:38 +0800

    删除README.MD

--- code/background/knox-web/src/main/java/com/knoxupc/knoxweb/KnoxWebApplication.java
+++ code/background/knox-web/src/main/java/com/knoxupc/knoxweb/KnoxWebApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KnoxWebApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args
+
+
+    ) {
         SpringApplication.run(KnoxWebApplication.class, args);
     }
 }
